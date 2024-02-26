d1cdaecc66ec0dcef7fb737c9db770d69cc7cc6b
Jan Romann <jan.romann@hs-emden-leer.de>
2024-02-26 14:24:32 +0100

    Render changelog in discovery ontology from RDF (#536)
    * Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * Adjust discovery ontology metadata
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF
    
    * fixup! Render changelog in discovery ontology from RDF

--- context/renderOntology.js
+++ context/renderOntology.js
@@ -4,13 +4,105 @@
 
 import { readFileSync, writeFileSync } from "fs";
 import sttl from "sttl";
-import { load, query } from "urdf";
+import { load, query, clear } from "urdf";
 
-const ontology = readFileSync("context/discovery-ontology.ttl", "utf-8");
+const initialVersionString = "Initial release. Placeholder HTML only."
+
+async function obtainOntologyMetaData(termIri) {
+  const template = `
+    template {
+      str(?value)
+    } where {
+    ?in a <http://www.w3.org/2002/07/owl#Ontology> ;
+        <${termIri}> ?value
+    }
+  `;
+
+  sttl.register(template);
+  const data = await sttl.applyTemplates();
+  return data;
+}
+
+async function obtainPriorVersionUrl() {
+  return obtainOntologyMetaData("http://www.w3.org/2002/07/owl#priorVersion");
+}
+
+async function obtainVersionInfo() {
+  return obtainOntologyMetaData("http://www.w3.org/2002/07/owl#versionInfo");
+}
+
+async function obtainChanges(options) {
+  const changes = await obtainOntologyMetaData(
+    "http://purl.org/vocab/vann/changes"
+  );
+
+  if (changes.length === 0 && options?.useDefault === true) {
+    return initialVersionString;
+  }
+
+  return changes;
+}
+
+async function obtainChangeLogEntry(options) {
+  const versionInfo = await obtainVersionInfo();
+  await reset();
+
+  const changes = await obtainChanges(options);
+  await reset({ fullReset: true });
+
+  return `<dt>${versionInfo}</dt><dd>${changes}</dd>`;
+}
+
+async function loadPriorVersion(priorVersion) {
+  const response = await fetch(priorVersion);
+  const ontology = await response.text();
+  await load(ontology);
+}
+
+async function reset(parameters) {
+  if (parameters?.fullReset === true) {
+    await clear();
+  }
+
+  sttl.clear();
+}
+
+function readTtlFile(filePath) {
+  return readFileSync(filePath, "utf-8");
+}
+
+async function obtainChangeLog() {
+  const changeLog = [];
+
+  const changeLogEntry = await obtainChangeLogEntry();
+  changeLog.push(changeLogEntry);
+
+  const currentOntology = readTtlFile("context/discovery-ontology.ttl");
+  await load(currentOntology);
+  let priorVersionUrl = await obtainPriorVersionUrl();
+
+  await reset({ fullReset: true });
+
+  while (priorVersionUrl != null && priorVersionUrl.length > 0) {
+    await loadPriorVersion(priorVersionUrl);
+
+    const changeLogEntry = await obtainChangeLogEntry({ useDefault: true });
+    changeLog.push(changeLogEntry);
+
+    await loadPriorVersion(priorVersionUrl);
+
+    priorVersionUrl = await obtainPriorVersionUrl();
+    await reset();
+  }
+
+  return changeLog;
+}
+
+const ontology = readTtlFile("context/discovery-ontology.ttl");
 await load(ontology);
 
-const sparqlTemplate = readFileSync("context/template.sparql", "utf-8");
-await sttl.register(sparqlTemplate);
+const sparqlTemplate = readTtlFile("context/template.sparql");
+sttl.register(sparqlTemplate);
 
 sttl.connect(async (sparqlQuery) => {
   const bindings = await query(sparqlQuery);
@@ -21,10 +113,19 @@ sttl.connect(async (sparqlQuery) => {
   };
 });
 
-const renderedOntology = await sttl.callTemplate("https://github.com/w3c/wot-discovery/ontology#main",
+let renderedOntology = await sttl.callTemplate(
+  "https://github.com/w3c/wot-discovery/ontology#main",
   {
     value: "discovery",
     type: "literal",
-  });
+  }
+);
 
-writeFileSync("context/discovery-ontology.html", renderedOntology)
+const changeLog = await obtainChangeLog();
+
+renderedOntology = renderedOntology.replace(
+  "%s",
+  changeLog.join("\n                ")
+);
+
+writeFileSync("context/discovery-ontology.html", renderedOntology);
